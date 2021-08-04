3c728446d9fabee10c0a53ec19ae2495519761eb
JAPinell <japinell@yahoo.com>
2021-08-04 00:00:23 -0500

    Added Main component containing the About component

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutHeading from "./AboutHeading";
+import AboutCard from "./AboutCard";
+
+function Main() {
+  return (
+    <main>
+      <AboutHeading />
+      <AboutCard />
+    </main>
+  );
+}
+
+export default Main;
