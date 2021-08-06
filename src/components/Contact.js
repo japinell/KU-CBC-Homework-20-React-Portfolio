cf308e8bf1391be7756935d0f645198304bdc5ee
JAPinell <japinell@yahoo.com>
2021-08-06 00:50:12 -0500

    Styled the Contact form (a little bit)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,59 +5,42 @@ import React from "react";
 //
 function Contact() {
   return (
-    <div class="container">
-      <form id="contact" action="" method="post">
-        <h1 className="display-6">Contact</h1>
-        <fieldset>
-          <input
-            placeholder="Your name"
-            type="text"
-            tabindex="1"
-            required
-            autofocus
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            placeholder="Your Email Address"
-            type="email"
-            tabindex="2"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            placeholder="Your Phone Number"
-            type="tel"
-            tabindex="3"
-            required
-          />
-        </fieldset>
-        <fieldset>
-          <input
-            placeholder="Your Web Site starts with http://"
-            type="url"
-            tabindex="4"
-            required
-          />
-        </fieldset>
-        <fieldset>
+    <div className="col-xs-12 col-sm-9 text-center-xs text-left-sm">
+      <h1>Contact Me</h1>
+      <form className="form login-form">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input className="form-control" type="text" id="name" name="name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input className="form-control" type="text" id="email" name="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="contents">Message:</label>
           <textarea
-            placeholder="Type your Message Here...."
-            tabindex="5"
-            required
+            className="form-control"
+            type="text"
+            id="contents"
+            name="contents"
+            rows="3"
           ></textarea>
-        </fieldset>
-        <fieldset>
+        </div>
+        <div className="form-group">
           <button
-            name="submit"
             type="submit"
-            id="contact-submit"
-            data-submit="...Sending"
+            className="btn btn-outline-primary btn-sm post-form-submit my-2 my-sm-0"
           >
-            Submit
+            Send
           </button>
-        </fieldset>
+          <a
+            className="btn btn-outline-primary btn-sm my-2 my-sm-0"
+            href="/"
+            role="button"
+          >
+            Cancel
+          </a>
+        </div>
       </form>
     </div>
   );
